a19d0499139f2361dc405a9d158d8f1c57538c9a
arsley <blue20will@gmail.com>
2018-11-09 01:03:10 +0900

    fix: Fix db:seed data of Teacher.create

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
 # create teacher as Administrator
-Teacher.create!(fullname: 'Smart Teacher', username: 'smat', password_digest: '200ceb26807d6bf99fd6f4f0d1ca54d4')
+# 'teacher'
+Teacher.create!(fullname: 'Smart Teacher', username: 'smat', password_digest: '50ecc45020be014e68d714cd076007e84a9621d9a5e589a916e45273014830b399d143a57f525554bfe9e751d97fe0fa884dbdea7b07721723b4eff39e9d28ad')
