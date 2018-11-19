86062a6a49f419cd1e40ba31af8eaa2fc9ea8df9
yoidea <yoidea.cc@gmail.com>
2018-11-19 23:26:11 +0900

    fix: Modify CORS header

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'smarteach.herokuapp.com', 'localhost:3000'
+    # origins 'smarteach.herokuapp.com', 'localhost:3000'
+    origins '*'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete]
